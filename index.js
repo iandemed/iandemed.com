aa1ebd1e4f8c13a951afb12576cbbcbd1764b337
iandemed <iandemed@gmail.com>
2020-11-02 23:50:02 -0500

    Now able to scroll from navbar links. Small style adjustments

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-
-
 // Toggle the display of our dropdown links
 const burgerDropdown = document.querySelector('#burger')
 const navbarMenu = document.querySelector('#nav-links')
+const navbarItems = document.querySelectorAll('.navbar-item')
 
 burgerDropdown.addEventListener('click', (e) => {
     navbarMenu.classList.toggle('is-active')
+
+    navbarItems.forEach(navbarItem => {
+        navbarItem.classList.toggle('has-text-black')
+    })
+    
     // The page automatically refreshes once the hamburger menu was clicked
     e.preventDefault()
 })
