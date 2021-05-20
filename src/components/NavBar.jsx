1f89f7452f01c77924b5476a25b7e61a84ada67f
Ian de Medeiros <44042532+iandemed@users.noreply.github.com>
2021-05-19 21:08:17 -0400

    Removed Navburger, left useRef in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,24 +11,8 @@ function NavBar(){
 
     const navbarMenu = useRef(null)
 
-    const handleClick = (e) => {
-        navbarMenu.current.classList.toggle("is-active")
-        navbarMenu.current.focus()
-        e.preventDefault()
-    }
-
-    const handleBlur = (e) => {
-        console.log(e.target)
-        e.target.removeClass("is-active")
-    }
-
     return(
         <nav className="navbar bg-primary-dark">
-            <div>
-            <NavBurger
-                handleClick={handleClick}
-            />
-            </div>
             <div 
                 className="navbar-menu" 
                 ref={navbarMenu}
